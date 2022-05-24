959e16147b615a0d276a21955cd8277cc06aee4f
Mahita Ramgolam <94129495+mahita098@users.noreply.github.com>
2022-05-24 15:45:46 +0400

    Add files via upload

---
+++ JAVASCRIPT 30days/Day 1/datatypes.js
@@ -0,0 +1,6 @@
+let myAge = 23
+let yourAge = 30
+
+console.log(myAge, yourAge)
+
+
---
+++ JAVASCRIPT 30days/Day 1/variable.js
@@ -0,0 +1,21 @@
+//declaring variables
+
+let firstName = 'Mahita'
+let lastName = 'Twin'
+let age = 23
+let country = 'Mauritius'
+let isSingle = true
+
+console.log(firstName, lastName, age, country, isSingle)
+
+/* declare 4 variables without assigninbg values:
+let
+const
+var
+
+let - for variable that changes at different time
+const - for data that doesnt change at all for ex country name.
+
+*/
+
+/* for typeoof try on console.log google */
