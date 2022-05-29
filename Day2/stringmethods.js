21982bb63662d56deb52e165dd3344b837b8de59
Mahita Ramgolam <94129495+mahita098@users.noreply.github.com>
2022-05-29 23:05:45 +0400

    Add files via upload

---
+++ Day2/stringmethods.js
@@ -0,0 +1,53 @@
+// lenght
+
+let jss = 'Javascript'
+console.log(jss.length)         // 10
+
+
+let myFirstName = 'Mahita'
+console.log(myFirstName.length)  // 6
+
+
+// accessing characters in a string
+// its counts as 0123456 not from the first letter 1 directly
+//start by zero
+// tell firstLetter to be string [0]
+
+let string = 'modem'
+let mdFirstLetter = string[0]
+
+console.log(mdFirstLetter)
+
+let secondLetter = string[1]       // o
+let thirdLetter = string[2]
+let lastLetter = string[4]       // m
+
+console.log(lastLetter)
+
+
+let lastIndex = string.length - 1
+
+console.log(lastIndex)  // 5-1 = 4
+
+console.log(string[lastIndex])  // m
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
