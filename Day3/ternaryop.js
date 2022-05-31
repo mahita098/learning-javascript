323af72efaad9a098b4b98403055379ab0491177
Mahita Ramgolam <94129495+mahita098@users.noreply.github.com>
2022-05-31 21:09:13 +0400

    Add files via upload

---
+++ Day3/ternaryop.js
@@ -0,0 +1,86 @@
+
+// Ternary operators ?:
+// ? if
+// : else
+
+let isRaining = true
+isRaining
+  ? console.log('You need a rain coat.')
+  : console.log('No need for a rain coat.')
+isRaining = false
+
+isRaining
+  ? console.log('You need a rain coat.')
+  : console.log('No need for a rain coat.')
+
+
+
+
+let number = 4
+number > 0
+    ? console.log(`${number} is a positive number`)
+    : console.log(`${number} is a negative number`)
+number = -4
+
+number > 0
+    ? console.log(`${number} is a positive number`)
+    : console.log(`${number} is a negative number`)
+
+
+
+
+//window promt 
+
+prompt('required text', 'optional text')
+let numbers = prompt('Enter number', 'number goes here')
+console.log(numbers)
+
+//comfirm method
+//A confirm box is often used to ask permission from a user to execute something. 
+
+const agree = confirm('Are you sure you like to delete? ')
+console.log(agree)
+
+// Creating a time object
+
+const now = new Date()
+console.log(now)
+console.log(now.getFullYear())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
