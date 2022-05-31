b727fb34d448e3ad0d4e7e4ee7ef0e231157fe7c
Mahita Ramgolam <94129495+mahita098@users.noreply.github.com>
2022-05-31 20:15:01 +0400

    Update stringmethods.js

--- Day2/stringmethods.js
+++ Day2/stringmethods.js
@@ -31,6 +31,31 @@ console.log(lastIndex)  // 5-1 = 4
 
 console.log(string[lastIndex])  // m
 
+//toUpperCase()
+//toLowerCase()
+
+console.log(string.toUpperCase())
+console.log(firstName.toUpperCase())
+
+console.log(string.toLowerCase())
+console.log(lastName.toLowerCase())
+
+//substr()
+
+console.log(string.substr(2,3))
+
+//split()
+
+let sstring = '40 days of javascript'
+
+console.log(sstring.split()) // change to an array []
+console.log(sstring.split('')) // splits word "" ""
+
+//trim()
+
+console.log(sstring)   //like if theres unnecessary spaces it removes it
+console.log(sstring.trim(' '))
+
 
 
 
