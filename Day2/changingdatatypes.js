96d809b36a9043fba3e6cdc94be3c11b937bac59
Mahita Ramgolam <94129495+mahita098@users.noreply.github.com>
2022-05-31 20:15:37 +0400

    Add files via upload

---
+++ Day2/changingdatatypes.js
@@ -0,0 +1,86 @@
+
+//string to int
+//parseInt()
+//Number()
+//plus sigh(+)
+
+
+
+
+let numo = '10'
+let numInt = parseInt(numo)
+console.log(numInt)
+
+
+let numIntt = Number(numo)
+
+console.log(numInt)
+
+
+// string to float
+
+let nuum = '9.81'
+let numfloat = parseFloat(nuum)
+console.log(numfloat)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
