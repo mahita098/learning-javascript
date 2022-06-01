45eb24ea190317a8202100fe418f79081a1fa108
Mahita Ramgolam <94129495+mahita098@users.noreply.github.com>
2022-06-01 19:25:23 +0400

    Add files via upload

---
+++ Day4/conditionals.js
@@ -0,0 +1,148 @@
+
+// conditionals
+/* using
+if
+if else
+if else if else
+switch
+ternary operators
+*/
+
+// to create an if condition, we need if keyword, condition inside a parenthesis and block of code inside a curly bracket({}).
+
+//using only if condition will display the if the condition is true and if not. nothing is displayed
+
+// use of 'if else"
+
+
+let num = 10
+if (num > 0) {
+    console.log(`${num} is a positive number`)     // 10 is a positive number
+} else {
+    console.log(`${num} is a negative number`)   //if number is less " # is a negative number"
+}      
+
+
+let sunIsShining = true
+if (sunIsShining) {
+    console.log('Remember to put sunscreen')
+} else {
+    console.log(`Remember to smile`)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
