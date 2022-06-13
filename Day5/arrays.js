49adc296974d59bde4d214e65dd1eef22d939b3e
Mahita Ramgolam <94129495+mahita098@users.noreply.github.com>
2022-06-13 21:38:34 +0400

    Add files via upload

---
+++ Day5/arrays.js
@@ -0,0 +1,121 @@
+const numbers = [0, 3.14, 9.81]
+const fruits = ['apple', 'mango', 'lemon']
+const vegetables = ['Potato', 'Cabbage', 'Carrot'] // array of strings, vegetables
+const animalProducts = ['milk', 'meat', 'butter'] // array of strings, products
+const webTechs = ['HTML', 'CSS', 'JS', 'React', 'Node'] // array of web technologies
+const countries = ['Finland', 'Denmark'] // array of strings, countries
+
+
+console.log('Numbers:', numbers)
+console.log('Fruit:', fruits)
+
+console.log('Number of numbers;', numbers.length)
+
+console.log('Number of fruits;', fruits.length)
+
+console.log('Number of countries;', countries.length)
+
+
+
+const arr = [
+    'Asabeneh',
+    250,
+    true,
+    { country: 'Finland', city: 'Helsinki' },
+    { skills: ['HTML', 'CSS', 'JS', 'React', 'Python'] }
+] // arr containing different data types
+
+console.log(arr)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
